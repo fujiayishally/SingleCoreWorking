907181a5002bdd75a72d3fef96f6badd6df77db7
fujiayishally <fujiayishally@gmail.com>
2019-07-23 18:45:04 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { Button } from "iview";
+import "iview/dist/styles/iview.css";
+
+Vue.config.productionTip = false;
+
+Vue.component("Button", Button);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
